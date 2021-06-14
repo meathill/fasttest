22f4503dc2c7a099d05f427c7f79aee2b5bcb87e
Meathill <meathill@gmail.com>
2021-06-14 21:10:19 +0800

    bugfix: fixed #13

--- src/index.js
+++ src/index.js
@@ -110,7 +110,7 @@ function showSpeed(speed) {
   }
   number.innerText = speed;
   clock.style.setProperty('--speed', speed);
-  const svg = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" height="354" width="354" viewBox="0 0 20 20" transform="rotate(135)"><circle r="5" cx="10" cy="10" fill="transparent" stroke="tomato" stroke-width="10" stroke-dasharray="calc(${speed} / 100 * 75 * 31.4 / 100) 31.4"></circle></svg>`;
+  const svg = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" height="354" width="354" viewBox="0 0 20 20" style="transform: rotate(135deg)"><circle r="5" cx="10" cy="10" fill="transparent" stroke="tomato" stroke-width="10" stroke-dasharray="calc(${speed} / 100 * 0.75 * 31.415926) 31.415926"></circle></svg>`;
   const blob = new Blob([svg], { type: 'image/svg+xml' });
   const url = URL.createObjectURL(blob);
   clockBg.style['-webkit-mask-image'] = 'url(' + url + ')';
